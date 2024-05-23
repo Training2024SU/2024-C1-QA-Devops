262f7b91848b738c655f910c3ec83f40245d621c
Devon Alvarez <osorioalvarez20@gmail.com>
2024-05-22 20:49:13 -0500

    Se agrega stepDefinition para servicio countryInformation

---
+++ jenkinsTaller/src/test/java/co/com/sofkau/stepDefinitions/service/CountryInfoSoapStepDefinitions.java
@@ -0,0 +1,25 @@
+package co.com.sofkau.stepDefinitions.service;
+
+import co.com.sofkau.stepDefinitions.SetupService;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CountryInfoSoapStepDefinitions extends SetupService {
+    @Given("que el es usuario quiere ingresar el codigo {string}")
+    public void queElEsUsuarioQuiereIngresarElCodigo(String string) {
+
+    }
+    @When("hace la peticion de tipo soap")
+    public void haceLaPeticionDeTipoSoap() {
+
+    }
+    @Then("deberia obtener un statuscode {int}")
+    public void deberiaObtenerUnStatuscode(Integer int1) {
+
+    }
+    @Then("deberia obtener el nombre de la capital del pais")
+    public void deberiaObtenerElNombreDeLaCapitalDelPais() {
+        
+    }
+}
